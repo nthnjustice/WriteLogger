52b4fb4e779716137b97a30cb392073a1cc327bf
Nathan Justice <n.justice@outlook.com>
2017-01-06 14:42:53 -0700

    push before dropping Bootstrap for Materialize

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+
+  resources :microposts, only: [:create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
