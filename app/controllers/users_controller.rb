ed127f77986e0176a333cded0f3c3d58f2dc91ea
Nathan Justice <n.justice@outlook.com>
2017-01-20 08:10:37 -0700

    styled profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.paginate(:page => params[:page], :per_page => 5)
     @posts = User.all.map { |u| u.microposts.first }
   end
 
