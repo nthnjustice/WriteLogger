cfb28cb57f2e3295ad063f2381766979b80e7f29
Nathan Justice <n.justice@outlook.com>
2017-02-09 07:42:11 -0700

    commenting and code formatting

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,46 +2,45 @@ class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
   has_many :goals, dependent: :destroy
   attr_accessor :remember_token
+  # name restrictions
+  validates :name, presence: true, length: { maximum: 25 }, uniqueness: { case_sensitive: false }
+  # email restrictions
   before_save { self.email = email.downcase }
-  validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  # password restrictions
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
-  # Returns the hash digest of the given string.
+  # returns the hash digest of the given string
   def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
-  # Returns a random token.
+  # returns a random token
   def User.new_token
     SecureRandom.urlsafe_base64
   end
 
-  # Remembers a user in the database for use in persistent sessions.
+  # remembers a user in the database for use in persistent sessions
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-  # Returns true if the given token matches the digest.
+  # returns true if the given token matches the digest
   def authenticated?(remember_token)
   	return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
-  # Forgets a user.
+  # forgets a user
   def forget
     update_attribute(:remember_digest, nil)
   end
 
-  # Defines a proto-feed.
-  # See "Following users" for the full implementation.
+  # defines a proto-feed
   def feed
     @feed_items = Micropost.all
     @feed_items = @feed_items.paginate(:page => 1, :per_page => 5)
